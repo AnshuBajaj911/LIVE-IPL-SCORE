9e0faa101327b7f54c07e4b45aa02a04983d95df
Anshu Bajaj <64094461+AnshuBajaj911@users.noreply.github.com>
2020-09-20 19:22:36 +0530

    Add files via upload

---
+++ ipl.js
@@ -0,0 +1,42 @@
+
+ 
+ function f1(){
+    
+var api = 'https://cricapi.com/api/cricketScore/?unique_id=';
+var unique_id ='1216493';
+var api_key = '&apikey=iiTJE2MoKMfHgUhMVm3E3QofjAB3';
+var url = api + unique_id + api_key;
+        fetch(url).then(
+            res=>{
+                res.json().then(
+                    data=>{
+                        
+                     console.log(data);
+                    
+                         var temp="";
+
+                        
+                             
+                             temp+="<tr class='table-success'>";
+                             temp+="<td>"+data.description+"</td>";
+                                temp+="<td>"+data.stat+"</td>";
+                            if(data.matchStarted == true)
+                             temp+="<td>"+data.score+"</td>";
+                            else 
+                             temp+="<td>"+'Match Not Started Yet'+"</td>";
+                             
+                           temp+="</tr>"
+                           
+
+                      
+
+                        
+                         document.getElementById("data").innerHTML += temp;
+                  
+                      
+                    
+                    }
+                )
+            }
+        ) }
+  
